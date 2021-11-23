115f3024446fd9b3ab99fdcca259b1339ad55c72
a-sh <iam.angie@ymail.com>
2021-11-23 16:19:24 +1100

    Setup Express and Nodemon dependencies

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-import express from "express";
+const express = require("express");
 
 // Initialise Express as an instance named 'app'
 const app = express();
+
+// Seperate these out in case of using Docker to wrap the app
+const PORT = 3000;
+const HOST = "0.0.0.0";
+
+// Standard route, sends back response
+app.get("/", (request, response) => {
+  response.send("Hello world!");
+});
+
+// Run the server by making it 'listen' for network traffic
+app.listen(PORT, HOST, () => {
+  console.log("Server is running!");
+});
